e6c2fabf359e7de9bbdac27614a79cb1cdc0328c
abcb2 <kouji.wtnb@gmail.com>
2016-07-22 23:59:01 +0900

    rename method

--- datetime_tests/test01.py
+++ datetime_tests/test01.py
@@ -4,7 +4,7 @@ import pytz
 
 
 class DateTimeTests(unittest.TestCase):
-    def test_delta_calc_converts_timezone_info_to_utc(self):
+    def test_calc_delta_different_timezone(self):
         self.assertTrue(True)
         dt_utc = datetime.datetime.now(tz=datetime.timezone.utc)
         dt_tokyo = datetime.datetime.now(
@@ -17,7 +17,7 @@ class DateTimeTests(unittest.TestCase):
         self.assertEqual(type(delta), datetime.timedelta)
         self.assertTrue(delta.total_seconds() < 1.0)
 
-    def test_timezone_info_replace(self):
+    def test_replace_timezone_info(self):
         dt_utc = datetime.datetime.now(tz=datetime.timezone.utc)
         self.assertEqual(dt_utc.tzname(), "UTC+00:00")
         dt_utc_replaced = dt_utc.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=9)))
@@ -27,7 +27,7 @@ class DateTimeTests(unittest.TestCase):
             dt_utc.strftime("%Y-%m-%d %H:%M:%S")
         )
 
-    def test_delta(self):
+    def test_delta_different_timezone(self):
         dt_now = datetime.datetime.now()
         dt_after = dt_now + datetime.timedelta(hours=9)
         self.assertEqual(dt_after - dt_now, datetime.timedelta(hours=9))
