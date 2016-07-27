20721733e4d8311e34cb7cf270e97e50601d588e
abcb2 <kouji.wtnb@gmail.com>
2016-07-27 13:27:39 +0900

    add test

--- datetime_tests/test02.py
+++ datetime_tests/test02.py
@@ -28,6 +28,20 @@ class PyTzTests(unittest.TestCase):
         delta = dt_utc - dt_tokyo
         self.assertEqual(delta, datetime.timedelta(hours=9))
 
+    def test_pytz_03(self):
+        dt_now = datetime.datetime.now()
+        self.assertEqual(dt_now.tzinfo, None)
+
+        with self.assertRaises(ValueError):
+            # can't use astimezone method to naive dt object
+            dt_tokyo = dt_now.astimezone(pytz.timezone('Asia/Tokyo'))
+
+        dt_utc = datetime.datetime(2016, 1, 1, 10, 0, 0, tzinfo=pytz.utc)
+        self.assertEqual(str(dt_utc), "2016-01-01 10:00:00+00:00")
+
+        dt_tokyo = dt_utc.astimezone(pytz.timezone('Asia/Tokyo'))
+        self.assertEqual(str(dt_tokyo), "2016-01-01 19:00:00+09:00")
+
 
 if __name__ == "__main__":
     unittest.main()
